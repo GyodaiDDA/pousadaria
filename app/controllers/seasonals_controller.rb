60eb776e1fe747cdafec705521c16c30d8237c29
Rodrigo Gyodai <rodrigo@modobrazil.com>
2023-11-06 15:06:11 -0300

    rollback - sem checagem de sobreposição

--- app/controllers/seasonals_controller.rb
+++ app/controllers/seasonals_controller.rb
@@ -13,12 +13,12 @@ class SeasonalsController < ApplicationController
   end
 
   def create
-    if date_overlap?
-      alert_message('O quarto já tem preços especiais no período.', 'new')
-    elsif Seasonal.new(seasonal_params).save
+    @seasonal = Seasonal.new(seasonal_params)
+    if @seasonal.save
       redirect_to @room, notice: 'Período adicionado com sucesso!'
     else
-      alert_message('Não foi possível adicionar o período.', 'new')
+      flash[:alert] = 'Não foi possível adicionar o quarto'
+      render 'new'
     end
   end
 
@@ -43,18 +43,5 @@ class SeasonalsController < ApplicationController
   def seasonal_params
     params.require(:seasonal)
           .permit(:name, :start_date, :end_date, :special_price, :room_id)
-  end
-
-  def date_overlap?
-    new_period = params[:start_date]..params[:end_date]
-    room_seasonals.each do |p|
-      old_period = p.start_date..p.end_date
-      return true if new_period.overlaps?(old_period)
-    end
-  end
-
-  def alert_message(message, screen)
-    flash[:alert] = message
-    render screen.to_s
   end
 end
--- spec/system/owner/seasonal/owner_creates_seasonal_spec.rb
+++ spec/system/owner/seasonal/owner_creates_seasonal_spec.rb
@@ -11,7 +11,7 @@ describe '::Owner cria novo Período' do
                      postal_code: '13200-000',
                      user_id: owner.id)
     @room = Room.create(name: 'Quarto Orlindgans', size: 30, max_guests: 2,
-                       base_price: 300, inn_id: inn.id)
+                        base_price: 300, inn_id: inn.id)
     login_as(owner)
     visit root_path
     click_on 'Minha Pousada'
@@ -42,24 +42,5 @@ describe '::Owner cria novo Período' do
     # Assert
     expect(current_path).to eq(room_path(1))
     expect(page).to have_content('Período adicionado com sucesso')
-  end
-
-  it 'does something' do
-    # Arrange
-    Seasonal.create(name: 'Friozinho Bom',
-                    start_date: '12/06/2024',
-                    end_date: '14/06/2024',
-                    special_price: '300',
-                    room_id: @room.id)
-    # Act
-    click_on 'Adicionar Período Especial'
-    fill_in 'Nome do Evento (opcional)', with: 'Dia dos Namorados'
-    fill_in 'Data de Início', with: '11/06/2024'
-    fill_in 'Data de Término', with: '14/06/2024'
-    fill_in 'Preço no Período', with: '200'
-    click_on 'Adicionar Período'
-    # Assert
-    expect(current_path).to eq(room_path(1))
-    expect(page).to have_content('O quarto já tem preços especiais no período.')
   end
 end
