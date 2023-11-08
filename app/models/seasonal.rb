702507a6a5dbbce3535df4999bb103c6621362a2
Rodrigo Gyodai <rodrigo@modobrazil.com>
2023-11-08 12:30:06 -0300

    Commit 9 - Full Sprint 1

--- app/models/seasonal.rb
+++ app/models/seasonal.rb
@@ -1,7 +1,5 @@
 class Seasonal < ApplicationRecord
   belongs_to :room
   validates :start_date, :end_date, :special_price, :room_id, presence: true
-  validates_with no_date_overlaping
-  validates_with no_backward_dates
-  validates_with no_bygone_dates
+  validates_with DatesValidator
 end
